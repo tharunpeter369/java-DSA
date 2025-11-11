3fb679e44bb7769828491b39f7bd9182d84994eb
tharunpeter123 <tharun.peter@deskday.ai>
2025-11-12 00:39:30 +0530

    find duplicate cycle sort done leetcode 287

---
+++ src/sort/A6FindAllDuplicateNumber287.java
@@ -0,0 +1,44 @@
+package sort;
+
+import java.util.Arrays;
+import java.util.List;
+public class A6FindAllDuplicateNumber287 {
+    public static void main(String[] args) {
+        int[] sampleArr = {1,1};
+        int ans = cyclicSort(sampleArr);
+//        System.out.println(Arrays.toString(ans)+"answer");
+        System.out.println(ans+"answer");
+
+
+    }
+        static int cyclicSort(int[] arr){
+        int i =0;
+        int count = 0;
+        int lastChange=-1;
+        while (i < arr.length) {
+            System.out.println(i+"fff"+arr[i]);
+            int correctIndex = arr[i]-1;
+            System.out.println(correctIndex+"rrrtttt"+arr[i]+arr[correctIndex]);
+            if(arr[i]!= arr[correctIndex]){
+                int temp = arr[correctIndex]; //value also 2    7v
+                arr[correctIndex] = arr[i]; // value 3 to index  arr[3] = 4
+                arr[i] = temp; // values 2 to index 1    7
+            }else{
+                if(i+1!= arr[i]){
+                    return arr[i];
+                }
+                i++;
+            }
+
+        }
+        return arr[0];
+//        List<Integer> resList = new ArrayList<>();
+//        // Loop and check condition
+//        for (int j = 0; j < arr.length; j++) {
+//            if (arr[j] != j + 1) {
+//                resList.add(j + 1);
+//            }
+//        }
+//        return resList;
+    }
+}
